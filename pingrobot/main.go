92f24288e7c1b6a6fe4e634d070b3de11d74606b
exttreme0001 <fpm.LukashovES@bsu.by>
2025-02-24 00:39:52 +0300

    -_-

---
+++ pingrobot/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/GOLANG-NINJA/pingrobot/workerpool"
+)
+
+const (
+	INTERVAL        = time.Second * 5
+	REQUEST_TIMEOUT = time.Second * 5
+	WORKERS_COUNT   = 2
+)
+
+var urls = []string{
+	"https://zhashkevych.com/",
+	"https://google.com/",
+	"https://golang.org/",
+}
+
+func main() {
+	results := make(chan workerpool.Result)
+	workerPool := workerpool.New(WORKERS_COUNT, REQUEST_TIMEOUT, results)
+
+	workerPool.Init()
+
+	go generateJobs(workerPool)
+	go proccessResults(results)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+
+	<-quit
+
+	workerPool.Stop()
+}
+
+func proccessResults(results chan workerpool.Result) {
+	go func() {
+		for result := range results {
+			fmt.Println(result.Info())
+		}
+	}()
+}
+
+func generateJobs(wp *workerpool.Pool) {
+	for {
+		for _, url := range urls {
+			wp.Push(workerpool.Job{URL: url})
+		}
+
+		time.Sleep(INTERVAL)
+	}
+}
